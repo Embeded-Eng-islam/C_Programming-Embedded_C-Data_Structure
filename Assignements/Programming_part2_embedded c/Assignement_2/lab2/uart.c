eb295ce7ea12472f2c25815cd92d5e32630f443a
Islam Zidane <109613655+Embeded-Eng-islam@users.noreply.github.com>
2022-09-16 16:58:30 +0200

    Delete Assignements/Programming_part2_embedded c/Assignement_2 directory
    Signed-off-by: Islam Zidane <109613655+Embeded-Eng-islam@users.noreply.github.com>

--- Assignements/Programming_part2_embedded c/Assignement_2/lab2/uart.c
+++
@@ -1,8 +0,0 @@
-#include"uart.h"
-#define uart0dr *((volatile unsigned int*)((unsigned int*)0x101f1000))
-void uart_tx_string(volatile unsigned char* ptr ){
-while(*ptr != '\0'){
-    uart0dr=(unsigned int)*ptr;
-    ptr++;
-    }
-}
--- Assignements/Programming_part2_embedded c/Assignement_2/lab2/uart.h
+++
@@ -1,4 +0,0 @@
-#ifndef _UART_H_
-#define _UART_H_
-void uart_tx_string(volatile unsigned char* ptr );
-#endif // _UART_H_
